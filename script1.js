c7353d383d8e8414711c1dbf93e22d82eaeef292
Jyothi1623 <jyothibsravana@gmail.com>
2025-10-05 11:17:59 +0530

    task1

---
+++ script1.js
@@ -0,0 +1,16 @@
+console.log("Welcome to the page")
+alert("This is javascript page")
+
+var name="Jyo"
+i="Sandy"
+
+
+console.log(i)
+let age=17
+let City="Hyderabad"
+name1="prabhas"
+console.log(name1)
+
+console.log(age)
+console.log(City)
+const state="Telangana"
