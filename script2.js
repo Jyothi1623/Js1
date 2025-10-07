95aa8708689239a8a4ec864317cdbaf641c0cb1f
Jyothi1623 <jyothibsravana@gmail.com>
2025-10-07 10:44:50 +0530

    script2

---
+++ script2.js
@@ -0,0 +1,32 @@
+var a=5
+var b=10
+a++;
+console.log("after incr:", a)
+console.log("add:", a+b)
+console.log("division", a/b )
+
+let p=10
+let q=5
+console.log("sub:", p-q)
+p--
+console.log("after dec:", p)
+
+const A=4
+const B=6
+console.log("multi:",A*B)
+
+
+var s=15
+var n="15"
+console.log("s==n:", s==n)
+console.log("s===n:", s===n)
+
+
+const i=13;
+if (i>=10 && i<=20){
+    console.log(i,"is between 10 and 20");
+}
+else{
+    console.log(i,"is not between 10 and 20")
+
+}
