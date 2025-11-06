b7da5d904f9829d647cb1e9c34d609ef3ba6c87c
Jyothi1623 <jyothibsravana@gmail.com>
2025-11-06 19:12:28 +0530

    dom1

---
+++ day1.js
@@ -0,0 +1,14 @@
+<script>
+    for(i=0;i<5;i++){
+    stars=" ";
+    for(k=0;k>5;k++){
+        stars+="  "
+    }
+    for(j=0;j<5;j++){
+        stars+=" *";
+        
+    }
+    console.log(stars)
+}
+
+</script>
